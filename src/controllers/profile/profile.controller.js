a04b22c371bc2b82ecf4fc707eb053c65145f2de
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-05-01 15:02:37 +0530

    added skill repository to user profile

--- src/controllers/profile/profile.controller.js
+++ src/controllers/profile/profile.controller.js
@@ -78,11 +78,107 @@ exports.handleGetUserProfileInfo = async (req, res) => {
                         as: "activities",
                     },
                 },
+                {
+                    $project: {
+                        _id: 0,
+                        userId: 0,
+                        password: 0,
+                        createdAt: 0,
+                        updatedAt: 0,
+                        __v: 0,
+                        "educations._id": 0,
+                        "educations.userId": 0,
+                        "educations.createdAt": 0,
+                        "educations.updatedAt": 0,
+                        "educations.__v": 0,
+                        "educations.verificationId": 0,
+                        "workexperiences._id": 0,
+                        "workexperiences.userId": 0,
+                        "workexperiences.createdAt": 0,
+                        "workexperiences.updatedAt": 0,
+                        "workexperiences.__v": 0,
+                        "workexperiences.verificationId": 0,
+                        "workexperiences.skills._id": 0,
+                        "licensecertifications._id": 0,
+                        "licensecertifications.userId": 0,
+                        "licensecertifications.createdAt": 0,
+                        "licensecertifications.updatedAt": 0,
+                        "licensecertifications.__v": 0,
+                        "licensecertifications.verificationId": 0,
+                        "licensecertifications.skills._id": 0,
+                        "projects._id": 0,
+                        "projects.userId": 0,
+                        "projects.createdAt": 0,
+                        "projects.updatedAt": 0,
+                        "projects.__v": 0,
+                        "projects.verificationId": 0,
+                        "projects.skills._id": 0,
+                        "activities._id": 0,
+                        "activities.userId": 0,
+                        "activities.createdAt": 0,
+                        "activities.updatedAt": 0,
+                        "activities.__v": 0,
+                        "activities.verificationId": 0,
+                    },
+                },
             ]);
 
-            const interestBasesSkills =
-                userProfileInfoResponse.interestBasedSkills;
-            console.log(interestBasesSkills);
+            let interestBasedSkills = {};
+            userProfileInfoResponse[0].interestBasedSkills.forEach((skill) => {
+                interestBasedSkills[skill] = { skill: skill, endorsedCount: 0 };
+            });
+
+            const workExperiences = userProfileInfoResponse[0].workexperiences;
+            const licenseCertifications =
+                userProfileInfoResponse[0].licensecertifications;
+            const projects = userProfileInfoResponse[0].projects;
+
+            let roleBasedSkills = {};
+
+            const extractSkillsAndCountEndorsements = (array) => {
+                array.forEach((item) => {
+                    item.skills.forEach((skill) => {
+                        const { name, endorsedBy } = skill;
+                        if (interestBasedSkills.hasOwnProperty(name)) {
+                            if (endorsedBy) {
+                                interestBasedSkills[name].endorsedCount++;
+                            }
+                        } else {
+                            if (!roleBasedSkills.hasOwnProperty(name)) {
+                                roleBasedSkills[name] = {
+                                    skill: name,
+                                    endorsedCount: 0,
+                                };
+                            }
+                            if (endorsedBy) {
+                                roleBasedSkills[name].endorsedCount++;
+                            }
+                        }
+                    });
+                });
+            };
+
+            extractSkillsAndCountEndorsements(workExperiences);
+            extractSkillsAndCountEndorsements(licenseCertifications);
+            extractSkillsAndCountEndorsements(projects);
+
+            interestBasedSkills = Object.values(interestBasedSkills);
+            roleBasedSkills = Object.values(roleBasedSkills);
+
+            const sortSkillsByEndorsement = (skillsArray) => {
+                skillsArray.sort((a, b) => b.endorsedCount - a.endorsedCount);
+                return skillsArray;
+            };
+
+            interestBasedSkills = sortSkillsByEndorsement(interestBasedSkills);
+            roleBasedSkills = sortSkillsByEndorsement(roleBasedSkills);
+
+            const skillRepository = {
+                interestBasedSkills: interestBasedSkills,
+                roleBasedSkills: roleBasedSkills,
+            };
+
+            userProfileInfoResponse[0].skillRepository = skillRepository;
 
             res.status(HttpStatusCode.Ok).json({
                 status: HttpStatusConstant.OK,
